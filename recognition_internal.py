faf9299b201aaec25283af1715b4ec4b677e740a
Himesh <nagar_himesh27@yahoo.com>
2020-11-27 21:40:18 +0530

    Updated

--- recognition_internal.py
+++ recognition_internal.py
@@ -9,7 +9,26 @@ def run():
     import cv2
     import numpy as np
 
-    # video_capture = cv2.VideoCapture(0)
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        # video_capture.open(0)
+        ret, frame = video_capture.read()
+        # print(video_capture.isOpened())
+
+        # print(ret)
+        # print(frame)
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    video_capture.release()
+    cv2.destroyAllWindows()
 
 
 b = tkinter.Button(master, text="Start", command=run)
